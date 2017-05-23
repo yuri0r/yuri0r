72632529bd4325e40f3a0c5c461ae4a6cec01aa1
yuri <macktrinz@yahoo.de>
2017-05-23 07:49:03 +0000

    added some js magic

---
+++ main.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var url = require("url");
+var http = require("http");
+var app;
+
+app = express();
+http.createServer(app).listen(3000);
+
+//making static files availeble
+app.use(express.static(__dirname +'/public'));
+
+app.get("/", function(req, res) {
+  ress.send("hey");	
+})
+
+
+//just some fun stuff stolen from exampels at scool
+app.get("/greetings", function(req, res) {
+	var query = url.parse(req.url, true).query;
+	var name = (query["name"] !== undefined) ? query["name"] : "Anonymous";
+	res.send("Greetings " + name);
+});
